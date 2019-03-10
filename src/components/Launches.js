236760c7d5abdad1434f50c5425e9f0efa4bd34f
scottdiemer <sdiemer@cfl.rr.com>
2019-03-09 23:03:31 -0500

    Added card border to reflect launch success and added Loading spinner

--- src/components/Launches.js
+++ src/components/Launches.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import LaunchItem from "./LaunchItem";
 import MissionKey from "./MissionKey";
+import Loading from "./Loading";
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -23,14 +24,14 @@ class Launches extends Component {
         <MissionKey />
         <Query query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
-            if (loading) return <h4>Loading...</h4>;
+            if (loading) return <Loading />;
             if (error) return console.log(error);
-            console.log(data);
 
             return (
               <Fragment>
                 {data.launches.map(launch => (
-                  <LaunchItem key={launch.flight_number} launch={launch} />
+                  // Mission name was used as key due to flight numbers being used more than once.
+                  <LaunchItem key={launch.mission_name} launch={launch} />
                 ))}
               </Fragment>
             );
